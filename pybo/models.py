db0d75132104b379b46f4cf8fbd2936196136823
potion-jack <flycast0080@gmail.com>
2023-04-16 18:59:32 +0900

    add answercreate and questioncreate

--- pybo/models.py
+++ pybo/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
-    create_date = models.DateTimeField()
-    
+    create_date = models.DateTimeField()    
     def __str__(self) -> str:
         return self.subject
 
