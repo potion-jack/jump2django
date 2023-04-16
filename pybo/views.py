f0a3fed705b4712e6343ab06091d9ffc9925b1b5
potion-jack <flycast0080@gmail.com>
2023-04-16 10:54:46 +0900

    add detail func

--- pybo/views.py
+++ pybo/views.py
@@ -8,3 +8,8 @@ def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list':question_list}
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question':question}
+    return render(request=request, template_name='pybo/question_detail.html', context=context)
