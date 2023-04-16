ce3bb3cf44a3b54b5bc412d84c1f265c6ca8fe23
potion-jack <flycast0080@gmail.com>
2023-04-16 10:38:18 +0900

    add pybo/admin QuestionAdmin class

--- pybo/admin.py
+++ pybo/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+admin.site.register(Question, QuestionAdmin)
